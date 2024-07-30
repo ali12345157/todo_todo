72895010d87907ca4e133e6c52b6baa6501173e8
Ali Ashraf Soliman <aliashraf152003@gmail.com>
2024-07-30 15:34:21 +0300

    main screen.

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+
+  runApp(Myapp());
+
+
+
+
+
+
+}
+class Myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw MaterialApp(debugShowCheckedModeBanner: false,);
+  }
+
+
+
+
+
+
+}
